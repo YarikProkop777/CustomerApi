8ca0dad0927f9562800fb7e17487521f5ccb818d
Yaroslav Prokopenko <misterrprokop@gmail.com>
2021-01-05 00:24:07 +0200

    Added EF Core support + implementation of the 'Repository' pattern

---
+++ CustomerApi.Data/Repository/v1/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CustomerApi.Data.Database;
+using CustomerApi.Domain.Entities;
+
+namespace CustomerApi.Data.Repository.v1
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(CustomerContext customerContext) : base(customerContext)
+        {
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await customerContext.Customers.FirstOrDefaultAsync(customer => customer.Id == id, cancellationToken);
+        }
+    }
+}
---
+++ CustomerApi.Data/Repository/v1/Repository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CustomerApi.Data.Database;
+
+namespace CustomerApi.Data.Repository.v1
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
+    {
+        protected readonly CustomerContext customerContext;
+
+        public Repository(CustomerContext customerContext)
+        {
+            this.customerContext = customerContext;
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            try
+            {
+                return customerContext.Set<TEntity>();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't retrieve entities: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            if(entity == null)
+            {
+                throw new ArgumentException($"{nameof(AddAsync)} entity must not be null");
+            }
+
+            try
+            {
+                await customerContext.AddAsync(entity);
+                await customerContext.SaveChangesAsync();
+
+                // we returned entity that was be saved previously
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} couldn't be saved: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentException($"{nameof(UpdateAsync)} entity must not be null");
+            }
+
+            try
+            {
+                customerContext.Update(entity);
+                await customerContext.SaveChangesAsync();
+
+                // we returned entity that was be updated previously
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} couldn't be updated: {ex.Message}");
+            }
+        }
+    }
+}
