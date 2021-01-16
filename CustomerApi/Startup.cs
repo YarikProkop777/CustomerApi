b6eb1512423652412b3cf0efd3d02d61bcd8bebd
Yaroslav Prokopenko <misterrprokop@gmail.com>
2021-01-16 12:59:04 +0200

    Modify 'CofigureServices' method

--- CustomerApi/Startup.cs
+++ CustomerApi/Startup.cs
@@ -1,15 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
+using CustomerApi.Data.Database;
+using CustomerApi.Data.Repository.v1;
+using CustomerApi.Domain.Entities;
+using CustomerApi.Models.v1;
+using CustomerApi.Service.v1.Command;
+using CustomerApi.Service.v1.Query;
+using CustomerApi.Validators.v1;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CustomerApi
 {
@@ -22,13 +38,72 @@ namespace CustomerApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddHealthChecks();
+            services.AddOptions();
+
+            services.AddDbContext<CustomerContext>(options => options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMvc().AddFluentValidation();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Customer API",
+                    Description = "A simple API to create or update customers",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Prokopenko Yaroslav",
+                        Email = "misterrprokop@gmail.com",
+                        Url = new Uri("https://github.com/YarikProkop777/CustomerApi")
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            // setup processing of invalid input model
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var actionExecutingContext =
+                        actionContext as ActionExecutingContext;
+
+                    if (actionContext.ModelState.ErrorCount > 0
+                        && actionExecutingContext?.ActionArguments.Count == actionContext.ActionDescriptor.Parameters.Count)
+                    {
+                        return new UnprocessableEntityObjectResult(actionContext.ModelState);
+                    }
+
+                    return new BadRequestObjectResult(actionContext.ModelState);
+                };
+            });
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            // register Repository services
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+
+            // register Validator services
+            services.AddTransient<IValidator<CreateCustomerModel>, CreateCustomerModelValidator>();
+            services.AddTransient<IValidator<UpdateCustomerModel>, UpdateCustomerModelValidator>();
+
+            // register CQRS services
+            services.AddTransient<IRequestHandler<CreateCustomerCommand, Customer>, CreateCustomerCommandHandler>();
+            services.AddTransient<IRequestHandler<UpdateCustomerCommand, Customer>, UpdateCustomerCommandHandler>();
+            services.AddTransient<IRequestHandler<GetCustomerByIdQuery, Customer>, GetCustomerByIdQueryHandler>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
